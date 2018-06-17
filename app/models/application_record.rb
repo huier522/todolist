88102967c4c5463286cb380c1bcb895f567fdf88
huier <andychen522.ambit@gmail.com>
2018-06-17 15:48:59 +0800

    put due date can't be in the past validate function to models/todolist.rb

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,11 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  validate :due_date_cannot_be_in_the_past
-  def due_date_cannot_be_in_the_past
-    if due_date.present? && due_date < Date.today
-      errors.add(:due_date, "can't be in the pest!")
-    end
-  end
-  
 end
--- app/models/todolist.rb
+++ app/models/todolist.rb
@@ -1,3 +1,10 @@
 class Todolist < ApplicationRecord
   validates_presence_of :name, :due_date, :note
+
+  validate :due_date_cannot_be_in_the_past
+  def due_date_cannot_be_in_the_past
+    if due_date.present? && due_date < Date.today
+      errors.add(:due_date, "can't be in the pest!")
+    end
+  end
 end
